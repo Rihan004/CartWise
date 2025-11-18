4a82259af8aa84799d267e5706ac3ef9e4a9be2f
Rihan Shaikh <rihanshaikh5598@gmail.com>
2025-11-18 21:05:29 +0530

    Google auth completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import ExpensePage from "./pages/ExpensePage";
 import GroceryPage from "./pages/GroceryPage";
 import GroceryTablePage from "./pages/GroceryTablePage";
+import AuthSuccess from "./pages/uthSuccess";
 function App() {
   return (
     <Router>
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <div className="p-6">
           <Routes>
+            <Route path="/auth/success" element={<AuthSuccess />} />
             <Route path="/" element={<ExpensePage />} />
             <Route path="/grocery" element={<GroceryPage />} />
             <Route path="/table-view" element={<GroceryTablePage />} />
